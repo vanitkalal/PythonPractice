5dba3c14454cebac9d62e071a787ce4e0106f756
vanitkalal <vanit.k937@gmail.com>
2020-01-20 06:13:37 +0000

    zigzag code

---
+++ zigzag_animation.py
@@ -0,0 +1,34 @@
+# This program generates and animated zigzag line
+
+import time
+
+def zigzag():
+    print('*****')
+    time.sleep(1)
+    print(' *****')
+    time.sleep(1)
+    print('  *****')
+    time.sleep(1)
+    print('   *****')
+    time.sleep(1)
+    print('    *****')
+    time.sleep(1)
+    print('     *****')
+    time.sleep(1)
+    print('      *****')
+    time.sleep(1)
+    print('     *****')
+    time.sleep(1)
+    print('    *****')
+    time.sleep(1)
+    print('   *****')
+    time.sleep(1)
+    print('  *****')
+    time.sleep(1)
+    print(' *****')
+    time.sleep(1)
+    print('*****')
+    time.sleep(1)
+
+while True:
+    zigzag()
