e419d94de55222ca511aea52ddfb89e03df33292
vanitkalal <vanit.k937@gmail.com>
2020-01-20 11:52:48 +0530

    Create VampireDairies.py

---
+++ VampireDairies.py
@@ -0,0 +1,12 @@
+print ('Traveller enter you name and age')
+name = input()
+age = input()
+
+if name == 'klaus':
+    print ('Hi klaus')
+elif int(age) <= 12:
+    print ('You cannot be The Mighty Klaus Mikhalson')
+elif int (age) >= 2000:
+    print ('Hello Klaus')
+else:
+    print ('Welcome stranger to the Mystic Falls')
