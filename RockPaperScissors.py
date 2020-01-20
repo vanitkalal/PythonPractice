df6158e11cce461d30cf9ff3b61e3352bb2f746d
vanitkalal <vanit.k937@gmail.com>
2020-01-20 11:52:11 +0530

    Create RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,10 @@
+#This code play rock paper scrissors with the user
+
+import random
+
+print('Lets play Rock Paper Scrissors, enter your selection: Rock [r], Paper [p], Scrissors [s]')
+choice = str(input())
+
+comp = random.randint(r, p, s)
+
+print(comp)
